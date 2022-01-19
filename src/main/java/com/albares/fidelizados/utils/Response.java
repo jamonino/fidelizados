0a292badf8a2234eba6ea74f97e5434081f74fde
josem <josem@LAPTOP-S1P10O6I>
2022-01-19 12:11:46 +0100

    init

--- src/main/java/com/albares/fidelizados/utils/Response.java
+++ src/main/java/com/albares/fidelizados/utils/Response.java
@@ -5,5 +5,37 @@ public class Response {
     private Integer responseCode;
     
     private GenericData genericData;
+
+    public Response() {
+    }
+
+    public Response(Integer responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public Response(Integer responseCode, GenericData genericData) {
+        this.responseCode = responseCode;
+        this.genericData = genericData;
+    }
+
+
+    public Integer getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(Integer responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public GenericData getGenericData() {
+        return genericData;
+    }
+
+    public void setGenericData(GenericData genericData) {
+        this.genericData = genericData;
+    }
+    
+    
+    
           
 }
